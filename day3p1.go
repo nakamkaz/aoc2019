4801a17573174025c390b8018f46a718a37c652c
nakamkaz <nakamkaz@kodain.com>
2019-12-12 00:04:54 +0900

    idea for day3p1 more

--- day3p1.go
+++ day3p1.go
@@ -34,3 +34,36 @@ func manhattan(x1,y1,x2,y2 int) int {
 	log.Printf("x1=%v y1=%v x2=%v y2=%v x2-x1=%v y2-y1=%v\n",x1,y1,x2,y2, x2-x1, y2-y1)
 	return int( math.Abs(float64(x2-x1)) + math.Abs(float64(y2-y1)) )
 }
+
+func p(a []string){
+var xv, yv int
+        for _, v := range line {
+                if strings.HasPrefix(v, "U") {
+                        t, _ := strconv.Atoi(strings.Replace(v, "U", "", 1))
+                        log.Println("U +",t)
+                        yv += t
+                        log.Println("Y ",yv)
+                }
+
+                if strings.HasPrefix(v, "D") {
+                        t, _ := strconv.Atoi(strings.Replace(v, "D", "", 1))
+                        log.Println("D -",t)
+                        yv -= t
+                        log.Println("Y ",yv)
+                }
+
+                if strings.HasPrefix(v, "R") {
+                        t, _ := strconv.Atoi(strings.Replace(v, "R", "", 1))
+                        log.Println("\t\tR +",t)
+                        xv += t
+                        log.Println("\t\tX ",xv)
+                }
+
+                if strings.HasPrefix(v, "L") {
+                        t, _ := strconv.Atoi(strings.Replace(v, "L", "", 1))
+                        log.Println("\t\tL -",t)
+                        xv -= t
+                        log.Println("\t\tX ",xv)
+                }
+        }
+}
