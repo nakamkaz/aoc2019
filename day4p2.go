fde7fe50af3c5c7181f7594b3890ac220dc2d6c5
nakamkaz <nakamkaz@kodain.com>
2019-12-18 00:26:27 +0900

    wrong result

--- day4p2.go
+++ day4p2.go
@@ -4,31 +4,31 @@ package main
    A-B
 */
 import (
-    "bufio"
-    "fmt"
-    "log"
-    //        "math"
-    "os"
-    "strconv"
-    "strings"
+	"bufio"
+	"fmt"
+	"log"
+	//        "math"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-    cntup := 0
-    stdin := bufio.NewScanner(os.Stdin)
-    stdin.Scan()
-    log.Println("line1: ", stdin.Text())
-    line := strings.Split(stdin.Text(), "-")
-    min, _ := strconv.Atoi(line[0])
-    max, _ := strconv.Atoi(line[1])
-    log.Println(min, max)
-    for k := min; k <= max; k++ {
-        if righteg(k) && onedouble(k) {
-            fmt.Println(k)
-            cntup++
-        }
-    }
-    fmt.Println("Answer: ", cntup)
+	cntup := 0
+	stdin := bufio.NewScanner(os.Stdin)
+	stdin.Scan()
+	log.Println("line1: ", stdin.Text())
+	line := strings.Split(stdin.Text(), "-")
+	min, _ := strconv.Atoi(line[0])
+	max, _ := strconv.Atoi(line[1])
+	log.Println(min, max)
+	for k := min; k <= max; k++ {
+		if righteg(k) && onedouble(k) {
+			fmt.Println(k)
+			cntup++
+		}
+	}
+	fmt.Println("Answer: ", cntup)
 }
 
 /**
@@ -42,7 +42,7 @@ func main() {
  1 2 3 4 55 OK
  3 44 4 66 Out
 
- DDDDD+ OK
+ DDDDD+ Out 222223
  DDDDEE OK
  DDD+++ Out
  +DDD++ Out
@@ -51,96 +51,96 @@ func main() {
  +++DDD Out
  AADDDD OK
  +DDDDD Out
+ 677889 Out
 
 **/
 
 func onedouble(i int) bool {
-    b := true
-    ia := inttoarray(i)
+	b := false
+	ia := inttoarray(i)
 
-    if (ia[0] == ia[1]) ||
-        ia[1] == ia[2] ||
-        ia[2] == ia[3] ||
-        ia[3] == ia[4] ||
-        ia[4] == ia[5] {
+	if (ia[0] == ia[1]) ||
+		ia[1] == ia[2] ||
+		ia[2] == ia[3] ||
+		ia[3] == ia[4] ||
+		ia[4] == ia[5] {
 
-        if ia[0] == ia[1] && ia[1] == ia[2] { // DDD+++ Out
-            b = false
-        }
-
-        if ia[0]==ia[1] && ia[1] == ia[2] && ia[2] == ia[3] { // DDDD++ OK
-            b= true
-        } else     if ia[1] == ia[2] && ia[2] == ia[3] { // +DDD++ Out
-            b = false
-        }
-
-        if ia[1] == ia[2] && ia[2] == ia[3] && ia[3] == ia[4] { // +DDDD+ Out
-            b = false
-        }
-
-        if ia[2] == ia[3] && ia[3] == ia[4] { // ++DDD+ Out
-            b = false
-        }
-
-        if ia[2] == ia[3] && ia[3] == ia[4] && ia[4] == ia[5] { // ++DDDD Out
-            b = true
-        } else if ia[3] == ia[4] && ia[4] == ia[5] { // +++DDD Out
-            b = false
-        }
-
-        if ia[1] == ia[2] && ia[2] == ia[3] && ia[3] == ia[4] && ia[4] == ia[5] { // +DDDDD Out
-            b = false
-        }
-        if ia[0] == ia[1] && ia[1] == ia[2] && ia[2] == ia[3] && ia[3] == ia[4] { // DDDDD+ OK
-            b = false
-        }
-
-        return b
-    } else {
-        b = false
-        return b
-    }
+		if ia[1] == ia[2] && ia[2] == ia[3] && ia[3] == ia[4] && ia[4] == ia[5] { // +DDDDD Out
+			b = false
+		} else if ia[0] == ia[1] && ia[1] == ia[2] && ia[2] == ia[3] && ia[3] == ia[4] { // DDDDD+ Out
+			b = false
+		} else if ia[0] == ia[1] && ia[1] == ia[2] && ia[2] == ia[3] { // DDDD++ OK
+			if ia[4] == ia[5] {
+				b = true
+			}
+		} else if ia[2] == ia[3] && ia[3] == ia[4] && ia[4] == ia[5] { // ++DDDD OK
+			if ia[0] == ia[1] {
+				b = true
+			}
+		} else if ia[0] == ia[1] && ia[1] == ia[2] { // DDD+++ Out
+			b = false
+		} else if ia[1] == ia[2] && ia[2] == ia[3] { // +DDD++ Out
+			b = false
+		} else if ia[1] == ia[2] && ia[2] == ia[3] && ia[3] == ia[4] { // +DDDD+ Out
+			b = false
+		} else if ia[2] == ia[3] && ia[3] == ia[4] { // ++DDD+ Out
+			b = false
+		} else if ia[3] == ia[4] && ia[4] == ia[5] { // +++DDD Out
+			b = false
+		} else if ia[1] == ia[2] { //+DD+++ Out
+			b = false
+		} else if ia[3] == ia[4] { //+++DD+ Out
+			b = false
+		} else if ia[0] == ia[1] { // DDEEFF
+			if ia[2] == ia[3] {
+				if ia[4] == ia[5] {
+					b = true
+				}
+			}
+		}
+	}
+	return b
 
 }
 
 func righteg(i int) bool {
 
-    ia := inttoarray(i)
+	ia := inttoarray(i)
 
-    if (ia[0] <= ia[1]) &&
-        ia[1] <= ia[2] &&
-        ia[2] <= ia[3] &&
-        ia[3] <= ia[4] &&
-        ia[4] <= ia[5] {
-        return true
-    } else {
-        return false
-    }
+	if (ia[0] <= ia[1]) &&
+		ia[1] <= ia[2] &&
+		ia[2] <= ia[3] &&
+		ia[3] <= ia[4] &&
+		ia[4] <= ia[5] {
+		return true
+	} else {
+		return false
+	}
 
 }
 
 func inttoarray(i int) []int {
 
-    var a []int = make([]int, 6)
+	var a []int = make([]int, 6)
 
-    a[5] = i % 10 / 1
-    log.Println("000001 ", i, a[5])
+	a[5] = i % 10 / 1
+	log.Println("000001 ", i, a[5])
 
-    a[4] = (i - a[5]*1) % 100 / 10
-    log.Println("000010 ", i, a[4])
+	a[4] = (i - a[5]*1) % 100 / 10
+	log.Println("000010 ", i, a[4])
 
-    a[3] = (i - a[4]*10 - a[5]*1) % 1000 / 100
-    log.Println("000100 ", i, a[3])
+	a[3] = (i - a[4]*10 - a[5]*1) % 1000 / 100
+	log.Println("000100 ", i, a[3])
 
-    a[2] = (i - a[3]*100 - a[4]*10 - a[5]*1) % 10000 / 1000
-    log.Println("001000 ", i, a[2])
+	a[2] = (i - a[3]*100 - a[4]*10 - a[5]*1) % 10000 / 1000
+	log.Println("001000 ", i, a[2])
 
-    a[1] = (i - a[2]*1000 - a[3]*100 - a[4]*10 - a[5]*1) % 100000 / 10000
-    log.Println("010000 ", i, a[1])
+	a[1] = (i - a[2]*1000 - a[3]*100 - a[4]*10 - a[5]*1) % 100000 / 10000
+	log.Println("010000 ", i, a[1])
 
-    a[0] = (i - a[1]*10000 - a[2]*1000 - a[3]*100 - a[4]*10 - a[5]*1) / 100000
-    log.Println("100000 ", i, a[0])
+	a[0] = (i - a[1]*10000 - a[2]*1000 - a[3]*100 - a[4]*10 - a[5]*1) / 100000
+	log.Println("100000 ", i, a[0])
 
-    return a
+	return a
 
 }
